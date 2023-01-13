6062cb47da7c507c137e4c4e68887c9928dcf7e2
bvelanor <bvelanor@emeal.nttdata.com>
2023-01-13 09:57:15 +0100

    Add new models to PensionPlan

---
+++ CalculatorProject/PensionPlan/Models/InputParametersPensionPlan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.PensionPlan
+{
+    public class InputParametersPensionPlan
+    {
+        public float TaxBase { get; set; }
+        public float IndividualPensionPlan { get; set; }
+        public float CompanyPensionPlan { get; set; }
+        public string CountryWhereDeclared { get; set; }
+
+        public InputParametersPensionPlan() { }
+
+        public InputParametersPensionPlan(float taxBase, float individualPensionPlan, float companyPensionPlan, string countryWhereDeclared)
+        {
+            TaxBase = taxBase;
+            IndividualPensionPlan = individualPensionPlan;
+            CompanyPensionPlan = companyPensionPlan;
+            CountryWhereDeclared = countryWhereDeclared;
+        }
+
+        public override string ToString()
+        {
+            return "PensionPlansModel: TaxBase(" + TaxBase
+                    + "), IndividualPensionPlan(" + IndividualPensionPlan
+                    + "), CompanyPensionPlan(" + CompanyPensionPlan
+                    + "), CountryWhereDeclared(" + CountryWhereDeclared + ").";
+        }
+    }
+}
---
+++ CalculatorProject/PensionPlan/Models/PensionPlan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CalculatorProject.PensionPlan
+{
+    public class PensionPlan
+    {
+        public float TaxBase { get; set; }
+        public float IndividualPensionPlan { get; set; }
+        public float CompanyPensionPlan { get; set; }
+        public string CountryWhereDeclared { get; set; }
+        public float TaxRelief { get; set; }
+
+        public PensionPlan(){}
+
+        public PensionPlan(float taxBase, float individualPensionPlan, float companyPensionPlan, string countryWhereDeclared, float taxRelief)
+        {
+            TaxBase = taxBase;
+            IndividualPensionPlan = individualPensionPlan;
+            CompanyPensionPlan = companyPensionPlan;
+            CountryWhereDeclared = countryWhereDeclared;
+            TaxRelief = taxRelief;
+        }
+
+        public override string ToString()
+        {
+            return  "PensionPlansModel: TaxBase(" + TaxBase 
+                    + "), IndividualPensionPlan(" + IndividualPensionPlan 
+                    + "), CompanyPensionPlan(" + CompanyPensionPlan
+                    + "), CountryWhereDeclared(" + CountryWhereDeclared
+                    + "), TaxRelief(" + TaxRelief + ").";
+        }
+    }
+}
