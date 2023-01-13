20d896b17500f6e4664c86cd7da3f0b7867b96c3
bvelanor <bvelanor@emeal.nttdata.com>
2023-01-13 10:03:26 +0100

    Add new Test to PensionPlanProgram

---
+++ CalculatorProjectTests/PensionPlan/Services/PensionPlan/AndorraPensionPlanCalculateTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorProject.PensionPlan.Services.PensionPlan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.PensionPlan.Services.PensionPlan.Tests
+{
+    [TestClass()]
+    public class AndorraPensionPlanCalculateTests
+    {
+        InputParametersPensionPlan inputParametersPensionPlans = new InputParametersPensionPlan();
+
+        [TestMethod()]
+        public void calculePensionPlanTest()
+        {
+            Assert.Fail("The method is not yet implemented");
+        }
+
+        [DataRow(22000, 0.00f)]
+        [DataRow(26000, 0.05f)]
+        [DataRow(46000, 0.10f)]
+        [DataTestMethod]
+        public void GetIrpfPercentageTest(float num1, float result)
+        {
+            var taxBase = num1; 
+            Assert.IsTrue(taxBase == result);
+        }
+
+        [TestMethod()]
+        public void GetTaxReliefTest()
+        {
+            Assert.Fail("The method is not yet implemented");
+        }
+    }
+}
---
+++ CalculatorProjectTests/PensionPlan/Services/PensionPlan/SpainPensionPlanCalculateTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorProject.PensionPlan.Services.PensionPlan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.PensionPlan.Services.PensionPlan.Tests
+{
+    [TestClass()]
+    public class SpainPensionPlanCalculateTests
+    {
+        [TestMethod()]
+        public void calculePensionPlanTest()
+        {
+            Assert.Fail("The method is not yet implemented");
+        }
+
+        [TestMethod()]
+        public void GetIrpfPercentageTest()
+        {
+            Assert.Fail("The method is not yet implemented");
+        }
+
+        [TestMethod()]
+        public void GetTaxReliefTest()
+        {
+            Assert.Fail("The method is not yet implemented");
+        }
+    }
+}
