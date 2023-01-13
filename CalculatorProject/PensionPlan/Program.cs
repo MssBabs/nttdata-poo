3a6bbee6f6b0c834bc3d44c08667f5f8174b0185
bvelanor <bvelanor@emeal.nttdata.com>
2023-01-13 09:49:31 +0100

    new main PensionPlan program

---
+++ CalculatorProject/PensionPlan/Program.cs
@@ -0,0 +1,32 @@
+﻿using CalculatorProject.PensionPlan.Services.OutputParameters;
+using CalculatorProject.PensionPlan.Services.PensionPlan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.PensionPlan
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            ConsoleOutputParameters.ShowWelcomeText();
+
+            InputParametersPensionPlan inputParametersPensionPlan = ConsoleInputParameters.GetInputParametersPensionPlan();
+
+            PensionPlanCalculate pensionPlanCalculate = null;
+            if (inputParametersPensionPlan.CountryWhereDeclared == "españa") 
+            {
+                pensionPlanCalculate = new SpainPensionPlanCalculate();
+            }
+            else 
+            {
+                pensionPlanCalculate = new AndorraPensionPlanCalculate();
+            }
+            PensionPlan pensionPlan = pensionPlanCalculate.calculePensionPlan(inputParametersPensionPlan);
+            ConsoleOutputParameters.ShowOutputParameters(pensionPlan.TaxRelief);
+        }
+    }
+}
