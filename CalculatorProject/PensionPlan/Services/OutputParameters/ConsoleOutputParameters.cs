8e5c03fcdfb149f21f3d824d07d675449bd14492
bvelanor <bvelanor@emeal.nttdata.com>
2023-01-13 10:01:03 +0100

    Add new OutputParameters services to PensionPlan

---
+++ CalculatorProject/PensionPlan/Services/OutputParameters/ConsoleOutputParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.PensionPlan.Services.OutputParameters
+{
+    public class ConsoleOutputParameters
+    {
+        public static void ShowWelcomeText()
+        {
+            Console.WriteLine("Bienvenido a Babs_Tax_Calculator");
+        }
+
+        public static void ShowOutputParameters(float taxRelief) 
+        {
+            Console.WriteLine("La desgravación final es de " + taxRelief);
+        }
+    }
+}
